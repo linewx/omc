46b7a3a6b7a466fcdafafdce7be551642a79646d
ganlin.lu <gan-lin.lu@microfocus.com>
2020-09-15 18:13:23 +0800

    add jmx operations

--- omt/resources/jmx/bean/bean.py
+++ omt/resources/jmx/bean/bean.py
@@ -13,3 +13,17 @@ class Bean(Resource, CmdTaskMixin):
         jmx = self.context['jmx']
         cmd = 'echo "open %s && beans"  | java -jar %s -n' % (jmx, jmxterm)
         self.run_cmd(cmd)
+
+    def info(self):
+        jmxterm = pkg_resources.resource_filename(__name__, '../../../lib/jmxterm-1.0.2-uber.jar')
+        jmx = self.context['jmx']
+        bean = self._get_resource_value()
+        cmd = 'echo "open %s && bean %s && info"  | java -jar %s -n' % (jmx, bean, jmxterm)
+        self.run_cmd(cmd)
+
+    def exec(self):
+        jmxterm = pkg_resources.resource_filename(__name__, '../../../lib/jmxterm-1.0.2-uber.jar')
+        jmx = self.context['jmx']
+        bean = self._get_resource_value()
+        cmd = 'echo "open %s && bean %s && info"  | java -jar %s -n' % (jmx, bean, jmxterm)
+        self.run_cmd(cmd)
