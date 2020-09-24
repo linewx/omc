57d8bcc888fc0526b03e23a4ea66c65b342f79cf
ganlin.lu <gan-lin.lu@microfocus.com>
2020-09-24 18:27:33 +0800

    add support for getting message

--- omt/resources/rmq/queue/queue.py
+++ omt/resources/rmq/queue/queue.py
@@ -7,3 +7,8 @@ class Queue(Resource):
         client.invoke_list('queues')
         # for name in list(map(lambda x: x.get('name'), client.get_queues())):
         #     print(name)
+
+    def get(self):
+        client = self.context['common']['client']
+        queue_name = self._get_resource_value()[0]
+        client.invoke_get(['queue='+ queue_name])
