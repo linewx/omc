11276dccbd569c31198ec62a33eaa85aaaf237e0
ganlin.lu <ganlin.lu@zkjin.com>
2020-10-11 15:29:22 +0800

    add ssh tunnel support

--- omt/resources/ssh/ssh.py
+++ omt/resources/ssh/ssh.py
@@ -7,7 +7,6 @@ from omt.core.resource import Resource
 
 
 class Ssh(Resource, CmdTaskMixin):
-    template = "ssh -nNT -L %(local_port)s:%(host)s:%(port)s %(bridge)s &"
 
     def _description(self):
         return 'SSH(Secure Shell) Smart Tool Set'
@@ -69,4 +68,23 @@ class Ssh(Resource, CmdTaskMixin):
 
         args = parser.parse_args(self._get_action_params())
         cmd = "scp %s %s:%s %s" % ('-r' if args.recursive else '', ssh_host, args.remote, args.local)
+        self.run_cmd(cmd)
+
+    def tunnel(self):
+        tunnel_template = "ssh -nNT -L %(local_port)s:%(remote_host)s:%(remote_port)s %(bridge)s"
+        bridge = self._get_one_resource_value()
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--local-port', nargs='?', help='local port')
+        parser.add_argument('--remote-port', nargs='?', help='remote port')
+        parser.add_argument('--remote-host', nargs='?', help='remote host')
+
+        args = parser.parse_args(self._get_action_params())
+        cmd = tunnel_template % {
+            'bridge': bridge,
+            'local_port': args.local_port,
+            'remote_port': args.remote_port,
+            'remote_host': args.remote_host,
+        }
+
         self.run_cmd(cmd)
