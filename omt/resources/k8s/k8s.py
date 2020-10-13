239a19fa955a2ff056aa47bbbb600f2ce78ef82f
ganlin.lu <gan-lin.lu@microfocus.com>
2020-10-13 08:51:53 +0800

    add k8s support

--- omt/resources/k8s/k8s.py
+++ omt/resources/k8s/k8s.py
@@ -19,11 +19,11 @@ class K8s(Resource, CmdTaskMixin):
             'client': v1
         }
 
-
-if __name__ == '__main__':
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+#
+# if __name__ == '__main__':
+#     config.load_kube_config()
+#     v1 = client.CoreV1Api()
+#     print("Listing pods with their IPs:")
+#     ret = v1.list_pod_for_all_namespaces(watch=False)
+#     for i in ret.items:
+#         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
--- omt/resources/k8s/pod/pod.py
+++ omt/resources/k8s/pod/pod.py
@@ -9,10 +9,20 @@ from omt.core.resource import Resource
 
 
 class Pod(Resource, CmdTaskMixin):
-    # def _completion(self, short_mode=True):
+    def _completion(self, short_mode=True):
+        super()._completion(True)
+
+        if not self._have_resource_value():
+            client = self.context['common']['client']
+            ret = client.list_pod_for_all_namespaces(watch=False)
+            self._print_completion([one.metadata.name for one in ret.items],True)
 
     def list(self):
         client = self.context['common']['client']
         ret = client.list_pod_for_all_namespaces(watch=False)
         for i in ret.items:
             print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+    def describe(self):
+
+
