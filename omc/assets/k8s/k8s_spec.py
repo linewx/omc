5860a4f5a8afbd1435751776d96d1d4c6508152c
ganlin.lu <ganlin.lu@zkjin.com>
2021-01-31 15:48:56 +0800

    k8s definition tree generator

--- omc/assets/k8s/k8s_spec.py
+++ omc/assets/k8s/k8s_spec.py
@@ -1,16 +1,67 @@
 import json
 
+
 # todo@rain
 # 1. pre-calculate resource spec for strategy patch merge
 # 2. calcuate for resource completion
 
+class K8sSpec:
+    def __init__(self, spec_file):
+        with open(spec_file) as f:
+            self.content = json.load(f)
+            self.definitions = self.content['definitions']
+
+    def _get_one_definition(self, resourceType):
+        return self.definitions.get(resourceType)
+
+    def _get_one_definition_properties(self, resourceType):
+        return self._get_one_definition(resourceType).get('properties')
+
+    def _get_one_definition_type(self, resourceType):
+        return self._get_one_definition(resourceType).get('type')
+
+    def gen_definition_tree(self, root_node):
+        properties = self._get_one_definition_properties(root_node)
+        results = {}
+        if properties:
+            for one_property_name, one_property_value in properties.items():
+                if '$ref' in one_property_value:
+                    results[one_property_name] = \
+                        {**self.gen_definition_tree(one_property_value['$ref'].replace('#/definitions/', '')),
+                         '_definition': one_property_value
+                         }
+                elif one_property_value.get('type') == 'array':
+                    items =  one_property_value['items']
+                    if '$ref' in  items:
+                        results[one_property_name] = {
+                            **self.gen_definition_tree(items['$ref'].replace('#/definitions/', '')),
+                            '_definition' : one_property_value
+                            }
+                    else:
+                        results[one_property_name] = {
+                            '_definition' : one_property_value
+                        }
+                else:
+                    results[one_property_name] = {
+                        '_definition': one_property_value
+                    }
+        else:
+            return {
+                '_definition': self._get_one_definition(root_node)
+            }
+        return results
+
+    def gen_sample(self, resource_type):
+        pass
+
+
+def pprint(obj):
+    print(json.dumps(obj, indent=2))
+
 
 if __name__ == '__main__':
-    content = {}
-    with open('/Users/luganlin/git/mf/omc/omc/fixtures/k8s/swagger.json') as f:
-        content = json.load(f)
+    spec = K8sSpec('/Users/luganlin/git/mf/omc/omc/assets/k8s/swagger.json')
 
-    for k,v in content['definitions'].items():
-        print(k)
-
-
+    # pprint(spec._get_one_definition_properties("io.k8s.api.apps.v1.Deployment"))
+    pprint(spec.gen_definition_tree("io.k8s.api.apps.v1.Deployment"))
+    # pprint(spec._get_one_definition_type("io.k8s.api.apps.v1.Deployment"))
