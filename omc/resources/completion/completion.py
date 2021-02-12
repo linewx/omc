d0a3f6928975e05875a1faf37d02a0d2f3d86a8a
ganlin.lu <ganlin.lu@zkjin.com>
2021-02-12 14:59:31 +0800

    fix completion issue

--- omc/resources/completion/completion.py
+++ omc/resources/completion/completion.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+import pkgutil
 
 import pkg_resources
 
 from omc.common import CmdTaskMixin
 from omc.config import settings
-from omc.core import Resource
+from omc.core import Resource, built_in_resources
 from omc.core.decorator import filecache
 
 
@@ -18,15 +19,28 @@ class Completion(Resource, CmdTaskMixin):
     def _get_resource_completion(self):
         results = []
         resources = (pkg_resources.resource_listdir('omc', 'resources'))
-        resources = list(filter(lambda x: x != '__init__.py' and x not in ['__pycache__'], resources))
-        for resource_type in resources:
+
+
+        # for built-in resources
+        for resource_type in built_in_resources:
+
             mod = __import__(".".join(['omc', 'resources', resource_type, resource_type]),
                              fromlist=[resource_type.capitalize()])
             clazz = getattr(mod, resource_type.capitalize())
             results.append(resource_type + ":" + clazz({})._description())
 
-        return "\n".join(results)
+        # for plugins
+        for finder,name,ispkg in pkgutil.iter_modules():
+            if name.startswith('omc_'):
+                resource_type = name.replace('omc_', '').lower()
+                mod = __import__(".".join(['omc_' + resource_type, resource_type]), fromlist=[resource_type.capitalize()])
+                clazz = getattr(mod, resource_type.capitalize())
+                results.append(resource_type + ":" + clazz({})._description())
 
+        # }
+
+
+        return "\n".join(results)
 
     def _run(self):
         if '--refresh' in self.context['all']:
